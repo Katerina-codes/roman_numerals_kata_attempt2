e4e0b993647ce4a7b0404861125b7cacaa23c5f5
katerina-codes <katerinaldg@gmail.com>
2017-04-03 10:30:53 +0100

    Changed formula to a loop which returns an string of Roman numerals.

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,17 +1,31 @@
 class RomanNumerals
 
   def convert_number(arabic)
-    if arabic < 4
-      numeral = ""
-        until arabic == 0
-          arabic -= 1
-          numeral << "I"
+    hash = {5 => "V", 1 => "I"}
+    numerals = ""
+
+    hash.each do |ara, rom|
+      while arabic > 0 && arabic >= ara
+           numerals << rom
+           arabic -= ara
         end
-      numeral
-    else
-      "V"
-    end  
+    end
+    puts numerals
+    numerals
   end
+    # if arabic < 4
+    #   numeral = ""
+    #     until arabic == 0
+    #       arabic -= 1
+    #       numeral << "I"
+    #     end
+    #   numeral
+    # elsif arabic == 5
+    #   "V"
+    # else
+    #   "VI"
+    # end
+
 
 
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -7,20 +7,24 @@ describe RomanNumerals do
     expect(number_converter.convert_number(arabic)).to eq(roman)
   end
 
-  it "returns I when arabic number is 1" do
+  it "returns I when arabic is 1" do
     expect_convert_number(1, "I")
   end
 
-  it "returns II when arabic number is 2" do
+  it "returns II when arabic is 2" do
     expect_convert_number(2, "II")
   end
-
-  it "returns III when arabic number is 3" do
+  
+  it "returns III when arabic is 3" do
     expect_convert_number(3, "III")
   end
 
-  it "returns V when arabic number is 5" do
-    expect_convert_number(4, "V")
+  it "returns V when arabic is 5" do
+    expect_convert_number(5, "V")
   end
+
+  # it "returns VI when arabic is 6" do
+  #   expect_convert_number(6, "VI")
+  # end
 
 end
