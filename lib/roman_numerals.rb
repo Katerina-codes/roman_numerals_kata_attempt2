3ca2d2d8ad067fd490284c454d6b2712e521aaa7
katerina georgiou <katerinaldg@gmail.com>
2017-04-04 09:13:11 +0100

    Tidied and renamed objects

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,16 +1,15 @@
 class RomanNumerals
 
-  def convert_number(arabic)
-    hash = {1000 => "M", 990 => "CMXC", 500 => "D", 100 => "C", 90 => "XC", 50 => "L", 40 => "XL", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I"}
+  def convert_number(arabic_to_convert)
+    arabic_to_roman = {1000 => "M", 990 => "CMXC", 500 => "D", 100 => "C", 90 => "XC", 50 => "L", 40 => "XL", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I"}
     numerals = ""
 
-    hash.each do |ara, rom|
-      while arabic > 0 && arabic >= ara
-           numerals << rom
-           arabic -= ara
+    arabic_to_roman.each do |arabic, roman|
+      while arabic_to_convert > 0 && arabic_to_convert >= arabic
+           numerals << roman
+           arabic_to_convert -= arabic
         end
     end
-    puts numerals
     numerals
   end
 
